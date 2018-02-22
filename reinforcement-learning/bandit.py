985eef476934c0a764c976abfa77eb8887242924
ralzq01 <ralzq01@163.com>
2018-02-22 22:33:42 +0800

    bandit alg

--- reinforcement-learning/bandit.py
+++ reinforcement-learning/bandit.py
@@ -1,12 +1,13 @@
 import numpy as np
 import sys
 import random
+import math
 
 np.random.seed(0)
 arm_num = 10
 arm_value = [0.0] * arm_num
 arm_Q = []
-times = 100000
+times = 10000
 
 def init():
     mu = 0
@@ -69,7 +70,44 @@ def e_greedy_alg():
     for e in arm_Q:
         sys.stdout.write('%.3f' % e + ' ')
     print ''
-    
+    print count_Q
+
+
+def ucb_alg():
+    """
+    Upper confidence bound algorithm, introduce a term for confidence level.
+    Often performs well but is not practical in reinforcement learning.
+    Use At = argmax(Qt(a) + c * sqrt(lnt / Nt(a))) for action
+    c * sqrt(lnt / Nt(a)) denotes confidence level
+    """
+    # init Q(a)
+    arm_Q = [0.0] * arm_num
+    count_Q = [0] * arm_num
+    c = 0.1
+    # ucb algorithms
+    for count in range(times):
+        # choose an arm
+        evl = [0.0] * arm_num
+        for i in range(arm_num):
+            if count_Q[i] == 0:
+                break
+            else:
+                evl[i] = arm_Q[i] + math.sqrt(c * math.log(count) / count_Q[i])
+        if i == arm_num - 1 and count_Q[i] != 0:
+            select = evl.index(max(evl))
+        else:
+            select = i
+        # get_reward
+        reward = get_reward(select)
+        # update Q(a)
+        count_Q[select] = count_Q[select] + 1
+        arm_Q[select] = arm_Q[select] + (reward - arm_Q[select]) / count_Q[select]
+    # print 10 arms final reward
+    print 'e-greedy algorithm result:'
+    for e in arm_Q:
+        sys.stdout.write('%.3f' % e + ' ')
+    print ''
+    print count_Q
 
 
 # init the 10 bandit
@@ -82,6 +120,8 @@ print ''
 greedy_alg()
 # use e-greedy algorithm
 e_greedy_alg()
+# use ucb algorithm
+ucb_alg()
 
 
 
